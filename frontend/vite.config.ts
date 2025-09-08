1f9d0e72f0bdfc533000fec9474172866ad84e30
Wertasile <am8704y@gre.ac.uk>
2025-09-08 03:39:47 +0300

    trying to solve dependecy errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  build: {
-    sourcemap: false, // ✅ generate sourcemaps for your code
-  },
-  esbuild: {
-    logOverride: { "unsupported-source-map": "silent" },
-  },
-  ssr: {
-    noExternal: ["socket.io-client"], // ensure it bundles safely
-  }
 });
